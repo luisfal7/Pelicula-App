c09a669f3ad39de0ff7f38f3abefa39f77591bb3
Luis Falconi <luism.falconi@gmail.com>
2023-01-19 18:13:30 -0300

    modif useMovies

--- src/hooks/useMovies.tsx
+++ src/hooks/useMovies.tsx
@@ -1,14 +1,46 @@
 import React, {useState, useEffect} from 'react';
-import {Movie, MovieDBNowPlaying} from '../interfaces/movieInterface';
+import { Movie, MovieDBNowResponse } from '../interfaces/movieInterface';
 import movieDB from '../api/movieDB';
 
+interface MoviesState {
+  nowPlaying: Movie[],
+  popular: Movie[],
+  topRated: Movie[],
+  upcoming: Movie[],
+}
+
 export const useMovies = () => {
+
     const [isLoading, setIsLoading] = useState(true)
-    const [peliculasEnCine, setPeliculasEnCine] = useState<Movie[]>([]);
+
+    const [movieState, setmovieState] = useState<MoviesState>({
+      nowPlaying:[],
+      popular:[],
+      topRated:[],
+      upcoming:[],
+    });
 
   const getMovies = async () => {
-    const resp = await movieDB.get<MovieDBNowPlaying>('/now_playing');
-    setPeliculasEnCine(resp.data.results);
+
+    const nowPlaying = movieDB.get<MovieDBNowResponse>('/now_playing');
+    const popular = movieDB.get<MovieDBNowResponse>('/popular');
+    const topRated = movieDB.get<MovieDBNowResponse>('/top_rated');
+    const upcoming = movieDB.get<MovieDBNowResponse>('/upcoming');
+
+    const response = await Promise.all([
+      nowPlaying,
+      popular,
+      topRated,
+      upcoming,    
+    ])
+
+    setmovieState({
+      nowPlaying: response[0].data.results,
+      popular: response[1].data.results,
+      topRated: response[2].data.results,
+      upcoming: response[3].data.results,
+    })
+
     setIsLoading(false)
   };
 
@@ -17,7 +49,7 @@ export const useMovies = () => {
   }, []);
 
   return {
-    peliculasEnCine,
+    ...movieState,
     isLoading
   };
 };
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,21 +1,28 @@
 import React from 'react';
 import Carousel from 'react-native-snap-carousel';
-import {Text, View, ActivityIndicator, Dimensions, FlatList, ScrollView} from 'react-native';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { MoviePoster } from '../components/MoviePoster';
+import {
+  Text,
+  View,
+  ActivityIndicator,
+  Dimensions,
+  FlatList,
+  ScrollView,
+} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {MoviePoster} from '../components/MoviePoster';
 import {useMovies} from '../hooks/useMovies';
-import { HorizontalSlider } from '../components/HorizontalSlider';
+import {HorizontalSlider} from '../components/HorizontalSlider';
 
-const {width: windowWidth} = Dimensions.get('window')
+const {width: windowWidth} = Dimensions.get('window');
 
 export const HomeScreen = () => {
-  const {peliculasEnCine, isLoading} = useMovies();
-  const {top} = useSafeAreaInsets()
+  const {nowPlaying, topRated, popular, upcoming, isLoading} = useMovies();
+  const {top} = useSafeAreaInsets();
 
   if (false) {
     return (
       <View style={{flex: 1, justifyContent: 'center', alignContent: 'center'}}>
-        <ActivityIndicator animating size="large" color="#00ff00"/>
+        <ActivityIndicator animating size="large" color="#00ff00" />
       </View>
     );
   }
@@ -23,19 +30,17 @@ export const HomeScreen = () => {
   return (
     <ScrollView>
       <View style={{marginTop: top + 20}}>
-        <Text>Home screen</Text>
-
         <View>
-          <Carousel 
-            data={peliculasEnCine}
-            renderItem={({item}: any) => (<MoviePoster movie={ item } />)}
-            sliderWidth={ windowWidth }
-            itemWidth={ 300 }
+          <Carousel
+            data={nowPlaying}
+            renderItem={({item}: any) => <MoviePoster movie={item} />}
+            sliderWidth={windowWidth}
+            itemWidth={300}
           />
         </View>
-
-      <HorizontalSlider movies={peliculasEnCine} title='En cine'/>
-
+        <HorizontalSlider movies={topRated} title="top rated" />
+        <HorizontalSlider movies={popular} title="popular" />
+        <HorizontalSlider movies={upcoming} title="upcoming" />
       </View>
     </ScrollView>
   );
