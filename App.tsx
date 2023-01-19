af10771af68a677ac026ecc4b6ddecabe39c48ac
Luis Falconi <luism.falconi@gmail.com>
2023-01-19 11:14:29 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Button, Text, View } from 'react-native'
+
+const App = () => {
+  return (
+    <View>
+      <Text>
+        hola mundo!
+      </Text>
+      <Button
+        title='hola'
+      />
+    </View>
+  )
+}
+
+export default App
